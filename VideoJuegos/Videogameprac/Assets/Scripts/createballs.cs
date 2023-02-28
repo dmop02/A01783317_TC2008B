1d79512cb44efe50d8ef9e8024cbd885cb3288e6
Domingo Mora <A01783317.tec.mx>
2023-02-28 12:51:35 -0600

    Cambio a videojuego

---
+++ VideoJuegos/Videogameprac/Assets/Scripts/createballs.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createballs : MonoBehaviour
+{
+    //Class variable to be changed from the Unity interface
+    [SerializeField] GameObject ball;
+    [SerializeField] float delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Call the Dropball function every 0.5 seconds
+        // and then keep calling it every set time
+        InvokeRepeating("Dropball", 0.5f, delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Dropball(){
+        // Generate a new random position
+        Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f),6,0);
+        //Create copy of prefab 
+        GameObject obj = Instantiate(ball, pos, Quaternion.identity);
+        //Boom object after certain period of time (This case 5 sec)
+        Destroy(obj, 5);
+    }
+}
